787804f5e65be5b38b364169a0c05c9c8265f3d2
tatabezhuashvili <tata.bezhuashvili1@gmail.com>
2016-05-14 01:52:52 -0700

    project birth

---
+++ New folder/Assets/Scr/Box_Script.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Box_Script : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.LeftArrow) && gameObject.transform.position.x >= -1.87f)
+		{
+		     gameObject.transform.Translate (-0.1f, 0, 0);
+		}
+		if (Input.GetKey (KeyCode.RightArrow) && gameObject.transform.position.x <= 1.87f) {
+			gameObject.transform.Translate (0.1f, 0, 0);
+		}
+}
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		Destroy (other.gameObject);
+		
+}
+}
